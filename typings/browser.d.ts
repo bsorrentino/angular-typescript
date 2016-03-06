45a095ae9f3a9afc0e4d9a583803fdd98c9deb42
bsorrentino <bartolomeo.sorrentino@gmail.com>
2016-03-07 00:34:13 +0100

    used typings instead of tsd

---
+++ typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser/ambient/angular/angular.d.ts" />
+/// <reference path="browser/ambient/angular/extend.d.ts"/>
+/// <reference path="browser/ambient/angular/angular-resource.d.ts" />
+/// <reference path="browser/ambient/jquery/jquery.d.ts" />
